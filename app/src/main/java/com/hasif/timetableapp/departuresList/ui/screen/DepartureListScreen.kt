b9cf5024ceadac394b1fb263d865d4b21265a1ca
Hasif Seyd <hasif.seyd009@gmail.com>
2024-08-05 21:46:52 +0200

    Add horizontal diviers between list item

--- app/src/main/java/com/hasif/timetableapp/departuresList/ui/screen/DepartureListScreen.kt
+++ app/src/main/java/com/hasif/timetableapp/departuresList/ui/screen/DepartureListScreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
@@ -16,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
@@ -95,6 +98,13 @@ fun DepartureListScreen(
                                     departure = item,
                                     modifier = Modifier.testTag("position=$position")
                                 )
+
+                                if (position < uiState.lastIndex)
+                                    HorizontalDivider(
+                                        color = Color.Gray,
+                                        thickness = 1.dp,
+                                        modifier = Modifier.padding(horizontal = 4.dp)
+                                    )
                             }
                         }
                     }
